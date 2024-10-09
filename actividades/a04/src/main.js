8b315fb77e6136b6e4da29a465d62042ac0884a9
inava <144198990+inava404@users.noreply.github.com>
2024-10-08 18:31:14 -0600

    Actividad a04 - Ejemplos 3 y 4 hechos

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -29,5 +29,51 @@ function ejemplo2(){
 }
 
 function ejemplo3(){
-    
+    var nombre;
+    var edad;
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    document.write('Hola ');
+    document.write(nombre);
+    document.write(', así que tienes ');
+    document.write(edad);
+    document.write(' años');
+}
+
+function ejemplo4(){
+    var valor1;
+    var valor2;
+    valor1 = prompt('Introducir primer número:', '');
+    valor2 = prompt('Introducir segundo número', '');
+    var suma = parseInt(valor1)+parseInt(valor2);
+    var producto = parseInt(valor1)*parseInt(valor2);
+    document.write('La suma es ');
+    document.write(suma);
+    document.write('<br>');
+    document.write('El producto es ');
+    document.write(producto);
+}
+
+function ejemplo5(){
+    var nombre;
+    var nota;
+    nombre = prompt('Ingresa tu nombre:', '');
+    nota = prompt('Ingresa tu nota:', '');
+    if (nota>5) {
+        document.write(nombre+' esta aprobado con un '+nota);
+    }
+}
+
+function ejemplo6(){
+    var num1,num2;
+    num1 = prompt('Ingresa el primer número:', '');
+    num2 = prompt('Ingresa el segundo número:', '');
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (num1>num2) {
+        document.write('El número mayor es '+num1);
+    }
+    else {
+        document.write('El número mayor es '+num2);
+    }
 }
