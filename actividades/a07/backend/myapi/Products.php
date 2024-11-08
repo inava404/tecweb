f105abe8603b016e1e35bad2de24d62c70350cd8
inava <144198990+inava404@users.noreply.github.com>
2024-11-08 13:10:37 -0600

    Search y Single - actividad 07

--- actividades/a07/backend/myapi/Products.php
+++ actividades/a07/backend/myapi/Products.php
@@ -44,12 +44,50 @@
             $this->conexion->close();
         }
 
-        public function search($id){
-            
+        public function search($parametro) {
+            if($parametro) {
+                $search = $parametro;
+                // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+                $sql = "SELECT * FROM productos WHERE (id = '{$search}' OR nombre LIKE '%{$search}%' OR marca LIKE '%{$search}%' OR detalles LIKE '%{$search}%') AND eliminado = 0";
+                if ( $result = $this->conexion->query($sql) ) {
+                    // SE OBTIENEN LOS RESULTADOS
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+                    if(!is_null($rows)) {
+                        // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                        foreach($rows as $num => $row) {
+                            foreach($row as $key => $value) {
+                                $this->data[$num][$key] = utf8_encode($value);
+                            }
+                        }
+                    }
+                    $result->free();
+                } else {
+                    die('Query Error: '.mysqli_error($this->conexion));
+                }
+            }
         }
 
-        public function single($id){
-            
+        public function single($id) {
+            if($id) {
+                $sql = "SELECT * FROM productos WHERE id = '{$id}'";
+
+                if ( $result = $this->conexion->query($sql) ) {
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+                    if(!is_null($rows)) {
+                        foreach($rows as $num => $row) {
+                            foreach($row as $key => $value) {
+                                $this->data[$num][$key] = utf8_encode($value);  // CODIFICA CADA CAMPO EN UTF-8
+                            }
+                        }
+                    }
+                    $result->free();
+                } else {
+                    die('Query Error: '.mysqli_error($this->conexion));
+                }
+                $this->conexion->close();
+            }
         }
 
         public function singleByName($id){
